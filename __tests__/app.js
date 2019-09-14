c1ad6d86c4a4dd4bec5886cc6d14b725bb3eef25
Adrian Gonzalo <gonzahh@gmail.com>
2019-09-14 03:45:54 +0200

    formatted and added basic tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,16 +1,28 @@
-'use strict';
-const path = require('path');
-const assert = require('yeoman-assert');
-const helpers = require('yeoman-test');
+"use strict";
+const path = require("path");
+const assert = require("yeoman-assert");
+const helpers = require("yeoman-test");
 
-describe('generator-agmdev-barebone:app', () => {
+describe("generator-agmdev-barebone:app", () => {
   beforeAll(() => {
     return helpers
-      .run(path.join(__dirname, '../generators/app'))
+      .run(path.join(__dirname, "../generators/app"))
       .withPrompts({ someAnswer: true });
   });
 
-  it('creates files', () => {
-    assert.file(['dummyfile.txt']);
+  it("creates files", () => {
+    assert.file(["README.md"]);
+    assert.file(["package.json"]);
+    assert.file(["LICENSE"]);
+    assert.file(["index.js"]);
+    assert.file([".gitignore"]);
+    assert.file([".eslintrc.js"]);
+    assert.file(["tests/example.spec.js"]);
+    assert.file(["src/app.js"]);
+    assert.file(["src/utils/index.js"]);
+    assert.file(["src/services/index.js"]);
+    assert.file(["src/models/index.js"]);
+    assert.file(["src/domain/index.js"]);
+    assert.file(["src/config/index.js"]);
   });
 });
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,165 +1,150 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
 
 const supportedLicenses = {
-  'MIT': 'LICENSE-MIT',
-  'GPL-3.0': 'LICENSE-GPL',
-}
+  MIT: "LICENSE-MIT",
+  "GPL-3.0": "LICENSE-GPL"
+};
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log(
-      yosay(`Holi!\n\nWelcome to the awesome ${chalk.red('agmdev-barebone')} generator!`)
+      yosay(
+        `Holi!\n\nWelcome to the awesome ${chalk.red(
+          "agmdev-barebone"
+        )} generator!`
+      )
     );
 
     const prompts = [
       {
-        type: 'input',
-        name: 'projectName',
-        message: 'Tell me the name of your new project',
-        default: 'PROJECT_NAME',
+        type: "input",
+        name: "projectName",
+        message: "Tell me the name of your new project",
+        default: "PROJECT_NAME"
       },
       {
-        type: 'input',
-        name: 'projectDescription',
-        message: 'Description of your new project',
-        default: 'PROJECT_DESCRIPTION',
+        type: "input",
+        name: "projectDescription",
+        message: "Description of your new project",
+        default: "PROJECT_DESCRIPTION"
       },
       {
-        type: 'input',
-        name: 'githubUsername',
-        message: 'Tell me your Github username',
-        default: 'agm-dev'
+        type: "input",
+        name: "githubUsername",
+        message: "Tell me your Github username",
+        default: "agm-dev"
       },
       {
-        type: 'input',
-        name: 'author',
-        message: 'Author of this project',
-        default: 'Adrián Gonzalo'
+        type: "input",
+        name: "author",
+        message: "Author of this project",
+        default: "Adrián Gonzalo"
       },
       {
-        type: 'list',
-        name: 'license',
-        message: 'Choose a license for this project',
+        type: "list",
+        name: "license",
+        message: "Choose a license for this project",
         choices: Object.keys(supportedLicenses),
-        default: 'MIT'
+        default: "MIT"
       },
       {
-        type: 'input',
-        name: 'keywords',
-        message: 'Add some keywords separated by comma',
-        default: 'node, nodejs',
-      },
+        type: "input",
+        name: "keywords",
+        message: "Add some keywords separated by comma",
+        default: "node, nodejs"
+      }
     ];
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
       this.props = props;
       this.log(`Initializing git repository...`);
-      this.spawnCommand('git', ['init']);
+      this.spawnCommand("git", ["init"]);
     });
   }
 
   writing() {
     // README.md
-    const readme = 'README.md';
-    this.fs.copyTpl(
-      this.templatePath(readme),
-      this.destinationPath(readme),
-      {
-        projectName: this.props.projectName,
-        githubUsername: this.props.githubUsername,
-      }
-    );
+    const readme = "README.md";
+    this.fs.copyTpl(this.templatePath(readme), this.destinationPath(readme), {
+      projectName: this.props.projectName,
+      githubUsername: this.props.githubUsername
+    });
 
     // LICENSE
-    if (this.props.license === 'GPL-3.0') {
+    if (this.props.license === "GPL-3.0") {
       this.fs.copy(
         this.templatePath(supportedLicenses[this.props.license]),
-        this.destinationPath('LICENSE')
+        this.destinationPath("LICENSE")
       );
     } else {
       this.fs.copyTpl(
         this.templatePath(supportedLicenses[this.props.license]),
-        this.destinationPath('LICENSE'),
+        this.destinationPath("LICENSE"),
         {
           author: this.props.author,
-          year: (new Date()).getFullYear(),
+          year: new Date().getFullYear()
         }
       );
     }
 
-    // index.js
-    const indexJs = 'index.js';
-    this.fs.copy(
-      this.templatePath(indexJs),
-      this.destinationPath(indexJs)
-    );
+    // Index.js
+    const indexJs = "index.js";
+    this.fs.copy(this.templatePath(indexJs), this.destinationPath(indexJs));
 
-    // example.test.js
-    const exampleTest = 'tests/example.spec.js';
+    // Example.test.js
+    const exampleTest = "tests/example.spec.js";
     this.fs.copy(
       this.templatePath(exampleTest),
       this.destinationPath(exampleTest)
     );
 
-    // src/app.js
-    const appJs = 'src/app.js';
-    this.fs.copy(
-      this.templatePath(appJs),
-      this.destinationPath(appJs)
-    );
+    // Src/app.js
+    const appJs = "src/app.js";
+    this.fs.copy(this.templatePath(appJs), this.destinationPath(appJs));
 
-    // src/config/index.js
-    const configJs = 'src/config/index.js';
-    this.fs.copy(
-      this.templatePath(configJs),
-      this.destinationPath(configJs)
-    );
+    // Src/config/index.js
+    const configJs = "src/config/index.js";
+    this.fs.copy(this.templatePath(configJs), this.destinationPath(configJs));
 
-    // src/domain/index.js
-    const empty = 'empty.js';
+    // Src/domain/index.js
+    const empty = "empty.js";
     this.fs.copy(
       this.templatePath(empty),
-      this.destinationPath('src/domain/index.js')
+      this.destinationPath("src/domain/index.js")
     );
 
-    // src/models/index.js
+    // Src/models/index.js
     this.fs.copy(
       this.templatePath(empty),
-      this.destinationPath('src/models/index.js')
+      this.destinationPath("src/models/index.js")
     );
 
-    // src/services/index.js
+    // Src/services/index.js
     this.fs.copy(
       this.templatePath(empty),
-      this.destinationPath('src/services/index.js')
+      this.destinationPath("src/services/index.js")
     );
 
-    // src/utils/index.js
+    // Src/utils/index.js
     this.fs.copy(
       this.templatePath(empty),
-      this.destinationPath('src/utils/index.js')
+      this.destinationPath("src/utils/index.js")
     );
 
     // .gitignore
-    const gitIgnore = '.gitignore';
-    this.fs.copy(
-      this.templatePath(gitIgnore),
-      this.destinationPath(gitIgnore)
-    );
+    const gitIgnore = ".gitignore";
+    this.fs.copy(this.templatePath(gitIgnore), this.destinationPath(gitIgnore));
 
     // .eslintrc.js
-    const eslintRc = '.eslintrc.js';
-    this.fs.copy(
-      this.templatePath(eslintRc),
-      this.destinationPath(eslintRc)
-    );
+    const eslintRc = ".eslintrc.js";
+    this.fs.copy(this.templatePath(eslintRc), this.destinationPath(eslintRc));
 
-    // writting dev dependencies to package.json
+    // Writting dev dependencies to package.json
     const packageJson = {
       devDependencies: {
         "codacy-coverage": "^3.4.0",
@@ -172,29 +157,29 @@ module.exports = class extends Generator {
         jest: "^24.9.0",
         nodemon: "^1.19.2",
         husky: "^3.0.5",
-        "lint-staged": "^9.2.5",
+        "lint-staged": "^9.2.5"
       },
-      keywords: this.props.keywords.split(',').map(i => i.trim().toLowerCase()),
-    }
+      keywords: this.props.keywords.split(",").map(i => i.trim().toLowerCase())
+    };
     this.fs.copyTpl(
-      this.templatePath('package.json'),
-      this.destinationPath('package.json'),
+      this.templatePath("package.json"),
+      this.destinationPath("package.json"),
       {
         projectName: this.props.projectName,
         projectDescription: this.props.projectDescription,
         author: this.props.author,
         license: this.props.license,
-        githubUsername: this.props.githubUsername,
+        githubUsername: this.props.githubUsername
       }
     );
-    this.fs.extendJSON(this.destinationPath('package.json'), packageJson);
+    this.fs.extendJSON(this.destinationPath("package.json"), packageJson);
   }
 
   install() {
     this.installDependencies({
       npm: true,
       bower: false,
-      yarn: false,
+      yarn: false
     });
   }
 
@@ -202,13 +187,13 @@ module.exports = class extends Generator {
     this.log(`Aaaand... that's all folks!\n`);
 
     this.log(`Checking linting rules...`);
-    this.spawnCommand('npm', ['run', 'lint']);
+    this.spawnCommand("npm", ["run", "lint"]);
 
     this.log(`Running tests...`);
-    this.spawnCommand('npm', ['run', 'test']);
+    this.spawnCommand("npm", ["run", "test"]);
 
     this.log(`Commit files...`);
-    this.spawnCommand('git', ['add', '.']);
-    this.spawnCommand('git', ['commit', '-m', 'initial commit']);
+    this.spawnCommand("git", ["add", "."]);
+    this.spawnCommand("git", ["commit", "-m", "initial commit"]);
   }
 };
